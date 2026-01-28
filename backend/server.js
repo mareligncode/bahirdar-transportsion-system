0abd86cb5efcf48125c91e7710d2df30fda91080
marelign <yimermarelign@gmail.com>
2026-01-28 14:16:16 +0300

    dbconnect

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,17 @@
 import express from 'express'
 import cors from 'cors'
 const app = express()
-const PORT=5000
+import connectdb from './config/database.js'
+const PORT = 5000
+connectdb()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({
-    extended:true
+    extended: true
 }))
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.send("wellcome")
-    
+
 })
 app.listen(PORT, () => {
     console.log(`server runing on the port http://localhost:${PORT}`)
