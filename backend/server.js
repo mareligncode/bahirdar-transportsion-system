40312202064ee28c144b774b7462f113d5d8bfe7
marelign <yimermarelign@gmail.com>
2026-01-28 15:41:01 +0300

    authfinal

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,18 @@
 import express from 'express'
 import cors from 'cors'
-const app = express()
-import connectdb from './config/database.js'
+import initSuperAdmin from './config/initSuperAdmin.js'
+import authRoutes from './routes/authRoutes.js'
+import connectDB from './config/database.js'
+connectDB()
+initSuperAdmin()
 const PORT = 5000
-connectdb()
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({
-    extended: true
-}))
-app.get("/", (req, res) => {
-    res.send("wellcome")
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-})
+app.use('/api/auth', authRoutes);
+
 app.listen(PORT, () => {
     console.log(`server runing http://localhost:${PORT}`)
 })
