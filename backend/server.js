98f073c3ad66f949ac03d1fbf72ce6eeb2f04bfc
marelign <yimermarelign@gmail.com>
2026-05-14 21:18:20 +0300

    feat: initialize Express server with Socket.io real-time location tracking and API routing

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,6 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
-import dotenv from 'dotenv'
-
-dotenv.config()
 import cors from 'cors'
 import helmet from 'helmet'
 import { rateLimit } from 'express-rate-limit'
